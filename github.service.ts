3b54e488fa602e7ca75d657e7f76468cac2d9da4
georgemunene <kengeorge010@gmail.com>
2020-04-13 14:59:16 +0300

    all

---
+++ github.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class GithubService {
+  private username = '';
+  private client_id = '';
+  private client_secret = '';
+
+  constructor(private http: HttpClient) {
+    console.log("Github service started");
+  }
+  getUser() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+      this.username +
+      "?client_id=" +
+      this.client_id +
+      "&client_secret=" +
+      this.client_secret
+    );
+  }
+  getRepos() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+      this.username +
+      "/repos" +
+      "?client_id=" +
+      this.client_id +
+      "&client_secret=" +
+      this.client_secret
+    );
+  }
+  updateUsername(username: any) {
+    this.username = username;
+  }
+}
